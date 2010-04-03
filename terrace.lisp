d1f90dd19adb6773b1107390c3afd339e9242ab2
unknown <Indy@.(none)>
2010-04-03 16:18:26 -0700

    Initial commit of terrace.
    Terrace is a cl based static html generator, it parses cl-who shtml and markdown to build websites.

---
+++ terrace.lisp
@@ -0,0 +1,70 @@
+;; Copyright (c) 2009, Nicholas "Indy" Ray. All rights reserved.
+;; See the LICENSE file for usage, modification, and distribution terms.
+(require 'cl-who)
+(require 'cl-markdown)
+
+(defparameter *file-config* "config.ter")
+
+(defparameter *template-file* nil)
+
+(defun make-local-path (file)
+  (merge-pathnames (pathname file) (current-directory)))
+
+(if (not (probe-file (make-local-path *file-config*)))
+    (progn
+      (format *standard-output* "Missing config file: ~A." *file-config*)
+      (quit)))
+
+(load (make-local-path *file-config*))
+
+(defvar *template* (let ((in (open (make-local-path *template-file*)))
+                         (html nil))
+                     (loop for line = (read in nil)
+                           while line do
+                           (setf html (cons line html)))
+                     (close in)
+                     html))
+
+;; from who.lisp
+(defun apply-to-tree (function test tree)
+  (declare (optimize speed space))
+  (declare (type function function test))
+  "Apply FUNCTION recursively to all elements of the tree TREE \(not
+only leaves) which pass TEST."
+  (cond
+    ((funcall test tree)
+      (funcall function tree))
+    ((consp tree)
+      (cons
+       (apply-to-tree function test (car tree))
+       (apply-to-tree function test (cdr tree))))
+    (t tree)))
+
+
+(mapcar
+ (lambda (file)
+   (let ((in (open file))
+         (out (open
+               (make-pathname :type "htm" :defaults file)
+               :direction :output :if-exists :supersede))
+         (markup ""))
+     (loop for line = (read-line in nil)
+           while line do
+           (setf markup (concatenate 'string markup line)))
+     (close in)
+     (eval (cl-who:tree-to-commands
+            (apply-to-tree
+             (lambda (x)
+               `(cl-who:str
+                 (let ((x
+                        ,(with-output-to-string
+                           (s)
+                           (cl-markdown:markdown markup :stream s))))
+                   x)))
+             (lambda (x) (eq x 'content))
+             *template*)
+            out))
+     (close out)))
+ (directory (make-local-path "*.pt")))
+
+(quit)
